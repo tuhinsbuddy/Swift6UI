a44e30d61bf13e7bf3c86761cdd07585561ef629
Tuhin S <tuhin.eios@gmail.com>
2025-08-13 00:17:11 +0530

    ZStack working

--- Swift6App/Components/StackView/Zee.swift
+++ Swift6App/Components/StackView/Zee.swift
@@ -16,22 +16,27 @@ struct ZeeStack: View {
     private let lineWidth: CGFloat = 5
     
     var body: some View {
-        ZStack(alignment: .center) {
-            Circle()
-                .fill(Color.red)
-                .stroke(.black, style: .init(lineWidth: lineWidth, lineCap: .butt, dash: [2, 2.5, 3, 3.5]))
-                .frame(width: width, height: self.height)
-            
-            Circle()
-                .fill(.green)
-                .stroke(.yellow, style: .init(lineWidth: 5, lineCap: .butt, dash: [2, 2.5, 3, 3.5]))
-                .frame(width: ((width * secondCircle) / 100), height: ((height * secondCircle) / 100))
-            
-            Circle()
-                .fill(.blue)
-                .stroke(.white, style: .init(lineWidth: 5, lineCap: .butt, dash: [2, 2.5, 3, 3.5]))
-                .frame(width: ((width * thirdCircle) / 100),
-                       height: ((height * thirdCircle) / 100))
+        VStack(spacing: 20) {
+            ZStack(alignment: .bottomLeading) {
+                Circle()
+                    .fill(Color.red)
+                    .stroke(.black, style: .init(lineWidth: lineWidth, lineCap: .butt, dash: [2, 2.5, 3, 3.5]))
+                    .frame(width: width, height: self.height)
+                
+                Circle()
+                    .fill(.green)
+                    .stroke(.yellow, style: .init(lineWidth: 5, lineCap: .butt, dash: [2, 2.5, 3, 3.5]))
+                    .frame(width: ((width * secondCircle) / 100), height: ((height * secondCircle) / 100))
+                
+                Circle()
+                    .fill(.blue)
+                    .stroke(.white, style: .init(lineWidth: 5, lineCap: .butt, dash: [2, 2.5, 3, 3.5]))
+                    .frame(width: ((width * thirdCircle) / 100),
+                           height: ((height * thirdCircle) / 100))
+            }
+            Text("3D Circle With ZStack!")
+                .foregroundStyle(.black)
+                .font(.largeTitle)
         }
     }
 }
