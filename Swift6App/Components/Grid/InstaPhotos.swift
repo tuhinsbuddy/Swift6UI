a5cf7960b530a79401545dcb7cd3a4839ee6d2fd
Tuhin S <tuhin.eios@gmail.com>
2025-09-06 16:20:32 +0530

    Working on lazy horizontal grid

---
+++ Swift6App/Components/Grid/InstaPhotos.swift
@@ -0,0 +1,20 @@
+//
+//  InstaPhotos.swift
+//  Swift6App
+//
+//  Created by Tuhin Samui on 06/09/25.
+//  Copyright © 2025 UltranLab. All rights reserved.
+//
+
+import SwiftUI
+
+struct InstaPhotos: View {
+    var body: some View {
+        
+    }
+    
+}
+
+#Preview {
+    InstaPhotos()
+}
--- Swift6App/Components/Grid/LazyH.swift
+++ Swift6App/Components/Grid/LazyH.swift
@@ -9,8 +9,18 @@
 import SwiftUI
 
 struct LazyH: View {
+    let row: [GridItem] = [
+        GridItem(.adaptive(minimum: 50, maximum: 100)),
+        GridItem(.adaptive(minimum: 100, maximum: 150))
+    ]
     var body: some View {
-//        LazyHGrid(rows: <#T##[GridItem]#>, content: <#T##() -> View#>)
+        ScrollView {
+            LazyHGrid(rows: row) {
+                ForEach(0..<100) { index in
+                    Rectangle().fill(.red).frame(width: 60, height: 100)
+                }
+            }
+        }
     }
 }
 
