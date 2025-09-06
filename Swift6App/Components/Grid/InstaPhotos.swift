6bf493036450f250c03411afec1e9ad25fc5cdb3
Tuhin S <tuhin.eios@gmail.com>
2025-09-06 23:03:41 +0530

    Small insta gallery like project.

--- Swift6App/Components/Grid/InstaPhotos.swift
+++ Swift6App/Components/Grid/InstaPhotos.swift
@@ -9,8 +9,32 @@
 import SwiftUI
 
 struct InstaPhotos: View {
+    let column: [GridItem] = [
+        GridItem(.flexible(), spacing: 5),
+        GridItem(.flexible(), spacing: 5),
+        GridItem(.flexible(), spacing: 5)
+    ]
     var body: some View {
-        
+        VStack(spacing: 43) {
+            Image("Desktop_Mac")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(height: 200)
+            ScrollView {
+                LazyVGrid(columns: column, spacing: 5, pinnedViews: .sectionHeaders) {
+                    Section {
+                        ForEach(0..<100) { index in
+                            Rectangle().fill((index % 2 == 0) ? .green : .black).frame(height: 100)
+                        }
+                    } header: {
+                        Text("Photos Posted").foregroundColor(.white)
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .background(.black)
+                    }
+                }
+            }
+        }
     }
     
 }
