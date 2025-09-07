b1f23811a138b213f5dbc1687aaf04c28863e46c
Tuhin S <tuhin.eios@gmail.com>
2025-09-07 21:12:10 +0530

    Working on the animation

---
+++ Swift6App/Animation/Anime.swift
@@ -0,0 +1,41 @@
+//
+//  Anime.swift
+//  Swift6App
+//
+//  Created by Tuhin Samui on 07/09/25.
+//  Copyright © 2025 UltranLab. All rights reserved.
+//
+
+import SwiftUI
+
+struct Anime: View {
+ @State var isAnimate: Bool = false
+    var body: some View {
+        ZStack() {
+            Color.black.ignoresSafeArea()
+            VStack() {
+                Spacer()
+                Text(isAnimate ? "Animated" : "Start").foregroundStyle(.white)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+                Button {
+                    withAnimation {
+                        isAnimate.toggle()
+                    }
+                } label: {
+                    Text(isAnimate ? "Reset" : "Animate")
+                        .font(.system(.title))
+                        .fontWeight(.bold)
+                        .foregroundStyle(isAnimate ? .blue : .white)
+                }
+//                Spacer()
+            }
+        }
+    }
+}
+
+
+#Preview {
+    Anime()
+}
